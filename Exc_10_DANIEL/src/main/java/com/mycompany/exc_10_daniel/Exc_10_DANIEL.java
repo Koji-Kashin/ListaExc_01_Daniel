e3b2b304b2dd80d465abe08ba1593a6e55acbd5f
Koji-Kashin <daniel.f.rodrigues6@aluno.senai.br>
2024-08-16 21:36:26 -0300

    commit update

---
+++ Exc_10_DANIEL/src/main/java/com/mycompany/exc_10_daniel/Exc_10_DANIEL.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exc_10_daniel;
+
+/**
+ *
+ * @author d.rodrigues
+ */
+public class Exc_10_DANIEL {
+
+    public static void main(String[] args) {
+        
+        int num = 10;
+        
+        while (num >0) {
+        num = num-1;
+            System.out.println(num);
+            
+        }
+        
+        
+    }
+}
